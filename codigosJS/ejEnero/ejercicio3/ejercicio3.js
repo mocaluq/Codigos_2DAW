514c6e2430a9aa3c3174d7ddd12d1c39bbf51dda
[Dmocaluq[ <monica.s333237@cesurformacion.com>
2024-01-27 01:03:15 +0100

    modificaciones

--- codigosJS/ejEnero/ejercicio3/ejercicio3.js
+++ codigosJS/ejEnero/ejercicio3/ejercicio3.js
@@ -4,7 +4,6 @@ Crea un texto y un botón que ponga buscar que muestre el CP y el nombre de la c
 que coincida o contenga el texto del campo creado. No importan las mayúsculas y minúsculas en la búsqueda. */
 
 //Creación del array con ciudades y códigos postales
-//var ciudades = {"Estepona":296980, "San Fernando":11100, "Malaga":29530, "Murcia":30004};
 var ciudades = {29680:"Estepona", 11100:"San Fernando", 29530:"Malaga", 30004:"Murcia"};
 function obtenerCodigoPostal() { 
     var busqueda = document.getElementById("buscar").value.toLowerCase();//Almaceno en una variable el valor introducido en el label
@@ -14,11 +13,11 @@ function obtenerCodigoPostal() {
         //En la condición paso la ciudad en cada itinerancia a minuscula 
         if (busqueda === ciudad) { //Comparamos si es igual la ciudad que introducimos en el label que la ciudad de cada itinerancia
             var resultado = document.getElementById("result"); //Creo una variable donde almacenaré el resultado que se mostrará en un parrafo
-            return resultado.textContent = "Ciudad: " + ciudades[codigoPostal] + ", Código postal: " + codigoPostal; 
+            return resultado.textContent = "Código postal: " + codigoPostal + ", Ciudad: " + ciudades[codigoPostal]; 
            // Salir del bucle si se encuentra la ciudad y mostramos la ciudad junto con el código postal
            
            /*si no hubiese creado un elemento p en el index3 podría mostrar el resultado con: 
-                    document.write("Ciudad: " + ciudades[codigoPostal] + ", Código postal: " + codigoPostal)*/
+                    document.write("Código postal: " + codigoPostal + ", Ciudad: " + ciudades[codigoPostal])*/
         }
     }
     
